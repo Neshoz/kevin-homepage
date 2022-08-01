dd25b77938b273741bc7e73af6de36c9a526cbd1
Neshoz <kevin.nemec94@gmail.com>
2022-08-02 00:52:26 +0200

    Remove unused imports

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,27 +1,17 @@
-import { Box } from '@chakra-ui/react';
-import { LinkItem, SocialLink } from '../Link';
-import { LinkedInIcon } from '../Icon/LinkedIn';
-import { GithubMarkIcon } from '../Icon/GithubMark';
-import HomeAnimation from '../../animations/home.json';
-import { LottieAnimation } from '../LottieAnimation';
+import { Box } from "@chakra-ui/react";
+import { LinkItem } from "../Link";
+import HomeAnimation from "../../animations/home.json";
+import { LottieAnimation } from "../LottieAnimation";
 
 const HomeLinkItem = () => {
   return (
     <LinkItem
       href="/"
       leftIcon={
-        <LottieAnimation
-          size={36}
-          animation={HomeAnimation}
-          loop={false}
-        />
+        <LottieAnimation size={36} animation={HomeAnimation} loop={false} />
       }
     >
-      <Box
-        display="flex"
-        flex={1}
-        alignItems="center"
-      >
+      <Box display="flex" flex={1} alignItems="center">
         Kevin Nemec
       </Box>
     </LinkItem>
@@ -40,18 +30,14 @@ export const Navbar = () => {
       w="100%"
       bg="#20202380"
       px={["6px", "200px"]}
-      css={{ backdropFilter: 'blur(10px)' }}
+      css={{ backdropFilter: "blur(10px)" }}
       zIndex={2}
     >
       <Box display="flex" alignItems="center" flex={1}>
         <HomeLinkItem />
-        <LinkItem href="/about">
-            About
-        </LinkItem>
-        <LinkItem href="/work">
-            Work
-        </LinkItem>
+        <LinkItem href="/about">About</LinkItem>
+        <LinkItem href="/work">Work</LinkItem>
       </Box>
     </Box>
   );
-}
+};
