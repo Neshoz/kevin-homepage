f326f37c0495fa5841db7665b6c95d4034d40c89
Neshoz <kevin.nemec94@gmail.com>
2022-08-29 16:14:03 +0200

    Add work

--- components/WorkGridItem/index.tsx
+++ components/WorkGridItem/index.tsx
@@ -1,8 +1,8 @@
 import NextImage, { StaticImageData } from "next/future/image";
 import { motion } from "framer-motion";
 import NextLink from "next/link";
-import { Box, Heading, Text } from "@chakra-ui/react";
-import { scaleTransition } from "../../util";
+import { IoCode } from "react-icons/io5";
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
 
 export interface WorkItem {
   id: string;
@@ -20,38 +20,46 @@ export const WorkGridItem = ({
   item: { id, thumbnail, title, description, scale },
 }: Props) => {
   return (
-    <NextLink href={`/works/${id}`}>
-      <Box
-        as={motion.div}
-        h={300}
-        w={["100%", 350]}
-        borderRadius={4}
-        bg="blackAlpha.500"
-        shadow="lg"
-        role="group"
-        overflow="hidden"
-        whileHover={scaleTransition}
-        _hover={{
-          cursor: "pointer",
-        }}
-      >
-        <Box h={150} bg="whiteAlpha.800">
-          <NextImage
-            src={thumbnail}
-            style={{
-              height: "100%",
-              objectFit: scale ?? "cover",
-              width: "100%",
-            }}
-          />
-        </Box>
-        <Box p={3}>
-          <Heading as="h2" fontSize={18} color="whiteAlpha.900" pb={4}>
-            {title}
-          </Heading>
-          <Text color="whiteAlpha.800">{description}</Text>
-        </Box>
+    <Flex
+      direction="column"
+      as={motion.div}
+      w={["100%", 350]}
+      borderRadius={4}
+      bg="blackAlpha.500"
+      shadow="lg"
+      role="group"
+      overflow="hidden"
+      _hover={{
+        cursor: "pointer",
+      }}
+    >
+      <Box h={150} bg="whiteAlpha.800">
+        <NextImage
+          src={thumbnail}
+          style={{
+            height: "100%",
+            objectFit: scale ?? "cover",
+            width: "100%",
+          }}
+        />
       </Box>
-    </NextLink>
+      <Flex direction="column" flexGrow={1} p={3}>
+        <Heading as="h2" fontSize={18} color="whiteAlpha.900" pb={4}>
+          {title}
+        </Heading>
+        <Text color="whiteAlpha.800">{description}</Text>
+      </Flex>
+      <Flex justify="flex-start" align="flex-end" px={3} py={3}>
+        <NextLink href={`/works/${id}`}>
+          <Button
+            lineHeight={0}
+            rightIcon={<IoCode style={{ marginTop: 2 }} />}
+            colorScheme="teal"
+          >
+            View work
+          </Button>
+        </NextLink>
+      </Flex>
+    </Flex>
   );
 };
--- pages/work.tsx
+++ pages/work.tsx
@@ -9,26 +9,65 @@ const items: WorkItem[] = [
     thumbnail: require("../public/images/logos/shb-logo.svg"),
     title: "Transaction Monitoring",
     description:
-      "Internal tool allowing personnel to monitor payments and take action on fraduluent ones",
+      "Developed an in-house application that was responsible for multiple payment routines stemming from and to the bank which led to the ability to generate automated investigatory actions.",
+  },
+  {
+    id: "public-login",
+    thumbnail: require("../public/images/logos/shb-logo.svg"),
+    title: "Public login",
+    description:
+      "Implemented an array of security inputs that interface with the public domain for ease of access and abandonment of legacy login methodologies.",
   },
   {
     id: "ideal-payments",
     thumbnail: require("../public/images/logos/shb-logo.svg"),
     title: "iDeal Payments (NL)",
-    description: "Direct payments via iDeal in The Netherlands",
+    description:
+      "Developed a public application to support direct payments via iDeal in The Netherlands, and an in-house application to search for and view details for payments via iDeal.",
   },
   {
-    id: "public-dashboard",
+    id: "conversation-api",
     thumbnail: require("../public/images/logos/sinch-logo2.png"),
-    title: "Public Dashboard",
+    title: "Omni Channel Conversation API",
     description:
-      "Various products for integrating with different communucation providers",
+      "Engineered an omni channel communication product which made it possible, from one place, to access and configure multiple channels and apis, which led to company growth by 15%.",
+  },
+  {
+    id: "rcs-public-dashboard",
+    thumbnail: require("../public/images/logos/sinch-logo2.png"),
+    title: "RCS Public Customer Dashboard",
+    description:
+      "Contributed to the RCS product on the public customer dashboard, for users to configure and communicate using the RCS, which made it possible to deprecate legacy channels such as SMS and MMS.",
+  },
+  {
+    id: "flowcharts",
+    thumbnail: require("../public/images/logos/sinch-logo2.png"),
+    title: "Node-RED Flowcharts",
+    description:
+      "Developed a flowchart tool based on node-red to visually program logic of sending messages via bots through various channels, integrated with one of the products on the public dashboard.",
+  },
+  {
+    id: "micro-frontend",
+    thumbnail: require("../public/images/logos/mediatool-logo-blue.svg"),
+    title: "Micro-frontend Infrastructure",
+    description:
+      "Designed and developed a micro-frontend infrastructure in favor of the current legacy app which made it possible for each team to own their domain and lead to faster and more robust feature development.",
+    scale: "scale-down",
+  },
+  {
+    id: "typescript-roadmap",
+    thumbnail: require("../public/images/logos/mediatool-logo-blue.svg"),
+    title: "TypeScript Roadmap",
+    description:
+      "Spearheaded the roadmap for modernizing the frontend infrastructure that allowed new features to be written in TypeScript and phase out JavaScript.",
+    scale: "scale-down",
   },
   {
     id: "mediatool",
     thumbnail: require("../public/images/logos/mediatool-logo-blue.svg"),
-    title: "Lead Frontend Engineer",
-    description: "Lead TypeScript integration and architectual decisions",
+    title: "Design System",
+    description:
+      "Lead a team of 4 to replace the current and considered legacy frontend component library which led to a more effective collaboration between the developers and the designers.",
     scale: "scale-down",
   },
 ];
